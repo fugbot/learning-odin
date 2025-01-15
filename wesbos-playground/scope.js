976dd3f0caf1182d607f0be843b1d6e34004baf7
Tania <tania.wang@statista.com>
2025-01-15 10:44:38 +0100

    First commit

---
+++ wesbos-playground/scope.js
@@ -0,0 +1,17 @@
+/* console.log('It works!');
+
+const first = 'wes';
+let second = 'bos';
+var age = 100;
+
+function sayHi(){console.log('hi!');} */
+
+const age = 100;
+function go() {
+    const hair = 'blonde';
+    console.log(hair);
+  }
+  
+  console.log(age);
+  go();
+  
